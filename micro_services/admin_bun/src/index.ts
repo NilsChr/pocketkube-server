f6b69666d6416976e7f27ae2ba6115e0e5ce0840
Nils Christian Bogen <nils.chr.bogen@gmail.com>
2023-10-08 09:45:55 +0200

    updated readme and removed comments

--- micro_services/admin_bun/src/index.ts
+++ micro_services/admin_bun/src/index.ts
@@ -6,67 +6,63 @@ import PocketBase from "pocketbase";
 import axios from "axios";
 import * as yaml from "js-yaml";
 import generateCompose from "./generateCompose";
-const fs = require('fs');
+const fs = require("fs");
 
 const composeFilePath = "./output/docker-compose.services.yml";
 const pb_url = "http://admin:8080";
 const pb = new PocketBase(pb_url);
 
-console.log('PASS', process.env || 'no password found');
+console.log("PASS", process.env || "no password found");
 
-const backendsSchema =  {
-  "id": "ob2i2yjehrsu9nt",
-  "name": "backends",
-  "type": "base",
-  "system": false,
-  "schema": [
-      {
-          "system": false,
-          "id": "1awpnbqi",
-          "name": "title",
-          "type": "text",
-          "required": false,
-          "presentable": false,
-          "unique": false,
-          "options": {
-              "min": null,
-              "max": null,
-              "pattern": ""
-          }
-      },
-      {
-          "system": false,
-          "id": "bkvuvuua",
-          "name": "inactive",
-          "type": "bool",
-          "required": false,
-          "presentable": false,
-          "unique": false,
-          "options": {}
+const backendsSchema = {
+  id: "ob2i2yjehrsu9nt",
+  name: "backends",
+  type: "base",
+  system: false,
+  schema: [
+    {
+      system: false,
+      id: "1awpnbqi",
+      name: "title",
+      type: "text",
+      required: false,
+      presentable: false,
+      unique: false,
+      options: {
+        min: null,
+        max: null,
+        pattern: ""
       }
+    },
+    {
+      system: false,
+      id: "bkvuvuua",
+      name: "inactive",
+      type: "bool",
+      required: false,
+      presentable: false,
+      unique: false,
+      options: {}
+    }
   ],
-  "indexes": [],
-  "listRule": null,
-  "viewRule": null,
-  "createRule": null,
-  "updateRule": null,
-  "deleteRule": null,
-  "options": {}
-}
-
+  indexes: [],
+  listRule: null,
+  viewRule: null,
+  createRule: null,
+  updateRule: null,
+  deleteRule: null,
+  options: {}
+};
 
 async function main() {
   await setupRootAdmin();
   const adminData = await pb.admins.authWithPassword(
-    process.env.ROOTADMIN || '',
-    process.env.ROOTADMINPW || ''
+    process.env.ROOTADMIN || "",
+    process.env.ROOTADMINPW || ""
   );
   const collections = await pb.collections.getFullList({ sort: "-created" });
-  //console.log("Got Collections");
-  //console.log(collections);
   const backendSchemaExists =
     collections.filter((c) => c.name === "backends")[0] || null;
-  //console.log("backendSchemaExists", backendSchemaExists);
   if (backendSchemaExists === null) {
     console.log("Creating schema: backends");
     await pb.collections.create(backendsSchema);
@@ -76,17 +72,19 @@ async function main() {
     console.log(e.action);
     console.log(e.record);
 
-    if(e.action === 'delete') {
-      fs.rename(`/pb_instances/${e.record.title}`, `/pb_instances/DELETED_${e.record.id}_${e.record.title}`, (err:any) => {
-        if (err) {
-          console.error(`Error renaming folder: ${err}`);
-        } else {
-          console.log('Folder renamed successfully');
+    if (e.action === "delete") {
+      fs.rename(
+        `/pb_instances/${e.record.title}`,
+        `/pb_instances/DELETED_${e.record.id}_${e.record.title}`,
+        (err: any) => {
+          if (err) {
+            console.error(`Error renaming folder: ${err}`);
+          } else {
+            console.log("Folder renamed successfully");
+          }
         }
-      });
+      );
     }
-
-
     generateOutput();
   });
   await generateOutput();
@@ -98,7 +96,7 @@ async function setupRootAdmin() {
   const payload = {
     email: process.env.ROOTADMIN,
     password: process.env.ROOTADMINPW,
-    passwordConfirm: process.env.ROOTADMINPW,
+    passwordConfirm: process.env.ROOTADMINPW
   };
 
   try {
@@ -108,8 +106,7 @@ async function setupRootAdmin() {
       }
     });
   } catch (error) {
-    console.log('Error when creating root admin');
-    //console.error("Error:", error);
+    console.log("Error when creating root admin");
   }
 }
 
@@ -120,13 +117,15 @@ async function generateOutput() {
     sort: "-title"
   });
 
-  const obj = generateCompose(records.map((r) => {
-    return {
-      id: r.id,
-      title: r.title
-    }
-  }));
+  const obj = generateCompose(
+    records.map((r) => {
+      return {
+        id: r.id,
+        title: r.title
+      };
+    })
+  );
   const yamlData = yaml.dump(obj);
-  console.log(yamlData)
+  console.log(yamlData);
   await Bun.write(composeFilePath, yamlData);
 }
